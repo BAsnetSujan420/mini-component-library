83a86738b1a041e22c457f7f73dddae09eff6435
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-01-19 15:51:10 +0545

    adds progressbar size variant

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,16 +5,41 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const STYLES = {
+  small: {
+    height: 8,
+  },
+  medium: {
+    height: 12,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+  },
+};
+
 const ProgressBar = ({ value, size }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to progressBar : ${size} `);
+  }
   return (
     <Wrapper
       role="progressbar"
       aria-valuenow={value}
       aria-valuemin={0}
       aria-valuemax={100}
+      style={{
+        "--padding": styles.padding + "px",
+      }}
     >
       <VisuallyHidden>{value}%</VisuallyHidden>
-      <Bar style={{ "--width": value + "%" }} />
+      <BarWrapper>
+        <Bar
+          style={{ "--width": value + "%", "--height": styles.height + "px" }}
+        />
+      </BarWrapper>
     </Wrapper>
   );
 };
@@ -23,12 +48,17 @@ const Wrapper = styled.div`
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: 4px;
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
   overflow: hidden;
 `;
 
 const Bar = styled.div`
   width: var(--width);
-  height: 8px;
+  height: var(--height);
   background-color: ${COLORS.primary};
   border-radius: 4px 0 0 4px;
 `;
