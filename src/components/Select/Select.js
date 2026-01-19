d1e56821a762b8ae25b75b8cca8fd15b624281b1
Sujan Basnet <basnetsujan1828420@gmail.com>
2026-01-19 16:59:56 +0545

    adds generic styles to select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>Hello World</PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  postion: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationalBit = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  padding: 12px 16px;
+`;
 
 export default Select;
